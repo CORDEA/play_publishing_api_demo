d8efbd0c6ed604fd4d215fb1ef99bbcdb3d8573c
Yoshihiro Tanaka <contact@cordea.jp>
2023-05-06 10:47:57 +0900

    list apks

--- main.py
+++ main.py
@@ -11,12 +11,25 @@ def list_bundles(client, name):
     return response['bundles']
 
 
+def list_apks(client, name):
+    response = client.edits().insert(packageName=name).execute()
+    edit_id = response['id']
+
+    response = client.edits().apks().list(editId=edit_id, packageName=name).execute()
+    return response['apks']
+
+
 def main(name):
     client = authorizer.authorize()
     bundles = list_bundles(client, name)
     for bundle in bundles:
         print(bundle['versionCode'])
         print(bundle['sha1'])
+
+    apks = list_apks(client, name)
+    for apk in apks:
+        print(apk['versionCode'])
+        print(apk['binary']['sha1'])
 
 
 if __name__ == '__main__':
